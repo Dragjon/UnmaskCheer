085927aac7a31bb1c278432f8c549979fa398e12
Dragjon <140328303+Dragjon@users.noreply.github.com>
2025-08-22 14:06:28 +0800

    add favicon

--- htdocs/leaderboard/leaderboard.php
+++ htdocs/leaderboard/leaderboard.php
@@ -27,6 +27,10 @@ sort($classes);
     <meta charset="UTF-8">
     <title>Leaderboard</title>
     <link rel="stylesheet" href="./styles.css">
+      <link rel="apple-touch-icon" sizes="180x180" href="../favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../favicon/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 </head>
 <body>
 
