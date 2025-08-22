106468d2184d8418d86fdfa144529847ae86621f
Dragjon <140328303+Dragjon@users.noreply.github.com>
2025-08-22 13:57:39 +0800

    Title edit

--- htdocs/index.php
+++ htdocs/index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Toxic Positivity Game</title>
+  <title>Unmask The Cheer</title>
   <link rel="stylesheet" href="./styles.css">
 </head>
 <body>
